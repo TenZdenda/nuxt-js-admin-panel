707b247a237c9b25b958d39336cb14fffc21bbb6
Zdeněk Pašek <me@zdenekpasek.cz>
2021-05-02 14:12:07 +0200

    admin layout

---
+++ components/Sidebar.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="bg-white shadow-xl">
+    <header class="p-5 md:p-10">
+      <h1 class="text-xl font-bold text-center md:hidden">AP</h1>
+      <h1 class="text-xl font-bold hidden md:block">Admin panel</h1>
+    </header>
+    <div>
+      <ul class="mx-5 md:mx-10">
+        <li class="py-5 md:flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="h-5 w-5 mx-auto md:mx-0"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+            />
+          </svg>
+          <p class="text-center md:ml-6">Home</p>
+        </li>
+        <li class="py-5 md:flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="h-5 w-5 mx-auto md:mx-0"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+            />
+          </svg>
+          <p class="text-center md:ml-6">Users</p>
+        </li>
+        <li class="py-5 md:flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="h-5 w-5 mx-auto md:mx-0"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+            />
+          </svg>
+          <p class="text-center md:ml-6">Settings</p>
+        </li>
+      </ul>
+
+      <button
+        class="bg-pink-400 text-white flex items-center px-8 py-2 rounded mx-auto hover:bg-pink-600 focus:outline-none"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M7 16l-4-4m0 0l4-4m-4 4h18"
+          />
+        </svg>
+        <p class="hidden md:block pl-3">Log out</p>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['open'],
+  data() {
+    return {}
+  },
+}
+</script>
---
+++ layouts/admin.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="h-screen flex">
+    <!-- Fixed sidebar -->
+    <Sidebar />
+    <!-- Scroll wrapper -->
+    <div class="flex-1 flex overflow-hidden">
+      <!-- Scrollable container -->
+      <div class="flex-1 overflow-y-scroll">
+        <div class="flex items-center justify-end px-6 py-4">
+          <div class="flex items-center">
+            <img
+              class="h-10 w-10 md:h-12 md:w-12 rounded-full"
+              src="https://zdenekpasek.com/images/me.jpg"
+              alt="Zdeněk"
+            />
+            <div class="ml-4">
+              <p class="font-bold">Zdeněk Pašek</p>
+              <span class="text-gray-500">hello@zdenekpasek.com</span>
+            </div>
+          </div>
+        </div>
+        <div class="p-6">
+          <Nuxt />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
