3d46bb1a71133ec098957296de66044978c280ba
Zdeněk Pašek <me@zdenekpasek.cz>
2021-05-04 02:41:53 +0200

    responsive sidebar menu redesign/fix

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,113 +1,142 @@
 <template>
   <div
-    class="flex flex-col justify-between h-screen shadow-md px-3 md:px-0 md:w-64"
+    class="w-64 bg-white shadow-lg px-5 py-8 space-y-16 absolute inset-y-0 left-0 transform md:relative md:translate-x-0 transition duration-200 ease-in-out"
+    :class="{ '-translate-x-full': bool }"
   >
-    <div class="px-10">
-      <header class="w-full my-14">
-        <h1 class="font-bold text-xl hidden md:block">Admin Panel</h1>
-        <h1 class="text-center font-bold text-xl md:hidden">AP</h1>
-      </header>
-
-      <ul class="w-full">
-        <li class="my-10 hover:text-pink-600">
-          <nuxt-link to="/admin/dashboard" class="flex items-center gap-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-              />
-            </svg>
-            <p class="font-semibold">Home</p>
-          </nuxt-link>
-        </li>
-        <li class="my-10">
-          <nuxt-link to="/admin/users" class="flex items-center gap-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            </svg>
-            <p class="font-semibold">Users</p>
-          </nuxt-link>
-        </li>
-        <li class="my-10">
-          <nuxt-link to="/admin/settings" class="flex items-center gap-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-              />
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
-            <p class="font-semibold">Settings</p>
-          </nuxt-link>
-        </li>
-      </ul>
+    <div class="flex justify-between space-x-5 px-4">
+      <div class="flex items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
+          />
+        </svg>
+        <h1 class="text-2xl font-extrabold">ZeLab</h1>
+      </div>
+      <button class="focus:outline-none" @click="closeMenu">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
     </div>
-
-    <div class="px-10">
-      <ul class="my-2 w-full">
-        <li class="py-6">
-          <button class="flex items-center w-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              width="20"
-              height="20"
-              class="mx-auto md:mx-5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M7 16l-4-4m0 0l4-4m-4 4h18"
-              />
-            </svg>
-            <p class="hidden md:block font-semibold">Sign off</p>
-          </button>
-        </li>
-      </ul>
-    </div>
+    <nav class="space-y-5">
+      <nuxt-link
+        to="/admin/dashboard"
+        class="flex items-center space-x-5 rounded transition duration-200 hover:bg-pink-700 hover:text-white px-4 py-2.5"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+          />
+        </svg>
+        <span class="font-semibold">Home</span>
+      </nuxt-link>
+      <nuxt-link
+        to="/admin/users"
+        class="flex items-center space-x-5 rounded transition duration-200 hover:bg-pink-700 hover:text-white px-4 py-2.5"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+          />
+        </svg>
+        <span class="font-semibold">Users</span>
+      </nuxt-link>
+      <nuxt-link
+        to="/admin/settings"
+        class="flex items-center space-x-5 rounded transition duration-200 hover:bg-pink-700 hover:text-white px-4 py-2.5"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+        </svg>
+        <span class="font-semibold">Settings</span>
+      </nuxt-link>
+    </nav>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['open'],
+  props: {
+    bool: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+  },
   data() {
     return {}
   },
+  watch: {
+    $route() {
+      this.closeMenu()
+    },
+  },
+  methods: {
+    closeMenu() {
+      this.$emit('close')
+    },
+  },
 }
 </script>
+
+<style scoped>
+.nuxt-link-active {
+  @apply bg-pink-700;
+  @apply text-white;
+}
+</style>
--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="h-screen flex">
     <!-- Fixed sidebar -->
-    <Sidebar />
+    <Sidebar :bool="close" @close="toggle" />
     <!-- Scroll wrapper -->
     <div class="flex-1 flex overflow-hidden">
       <!-- Scrollable container -->
       <div class="flex-1 overflow-y-scroll">
-        <div class="flex items-center justify-end px-6 py-4">
+        <div class="flex items-center justify-between md:justify-end px-6 py-4">
+          <button class="focus:outline-none md:hidden" @click="toggle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </button>
           <nuxt-link
             to="/admin/profile"
             class="flex items-center p-2 hover:bg-gray-100 rounded-full"
@@ -31,5 +47,16 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      close: true,
+    }
+  },
+  methods: {
+    toggle() {
+      this.close = !this.close
+    },
+  },
+}
 </script>
