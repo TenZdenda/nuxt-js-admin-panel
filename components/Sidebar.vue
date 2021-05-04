701ad85417ead592aebc8b431914d7e881d5b2e1
Zdeněk Pašek <me@zdenekpasek.cz>
2021-05-04 21:55:03 +0200

    Log out button

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -93,6 +93,28 @@
         <span class="font-semibold text-md">Settings</span>
       </nuxt-link>
     </nav>
+    <hr class="border-gray-700" />
+    <div>
+      <button
+        class="w-full px-4 py-2.5 text-left text-white flex space-x-5 rounded transition duration-200 hover:bg-white hover:bg-opacity-20 hover:text-gray-100 hover:shadow-xl"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+          />
+        </svg>
+        <span>Log out</span>
+      </button>
+    </div>
   </div>
 </template>
 
